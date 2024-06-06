f4ca7bf8600a4b14a87cd89b3ceea461db38d521
fitrawildania <wildafitra853@gmail.com>
2024-06-06 22:21:51 +0700

    capstone

---
+++ app/src/main/java/com/myrent/capstoneproject/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.myrent.capstoneproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.myrent.capstoneproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNav: BottomNavigationView = binding.bottomNav
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_bottom_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_order, R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNav.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/myrent/capstoneproject/ui/order/OrderActivity.kt
@@ -0,0 +1,12 @@
+package com.myrent.capstoneproject.ui.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.myrent.capstoneproject.R
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+    }
+}
